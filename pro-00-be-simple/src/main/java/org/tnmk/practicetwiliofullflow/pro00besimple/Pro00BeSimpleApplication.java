6510abe650d7c5038057cf43435738a2e958cbb1
trank <Kevin.Tran@pointclickcare.com>
2021-03-30 18:25:42 -0400

    + Fix logback setting.

--- pro-00-be-simple/src/main/java/org/tnmk/practicetwiliofullflow/pro00besimple/Pro00BeSimpleApplication.java
+++ pro-00-be-simple/src/main/java/org/tnmk/practicetwiliofullflow/pro00besimple/Pro00BeSimpleApplication.java
@@ -1,12 +1,15 @@
 package org.tnmk.practicetwiliofullflow.pro00besimple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.invoke.MethodHandles;
+
 @SpringBootApplication
 public class Pro00BeSimpleApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Pro00BeSimpleApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Pro00BeSimpleApplication.class, args);
+  }
 }
