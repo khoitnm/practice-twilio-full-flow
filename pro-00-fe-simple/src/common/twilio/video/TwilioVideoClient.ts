465fd9ac577cd82c631f1d7fb2462e016bdd7914
trank <Kevin.Tran@pointclickcare.com>
2021-03-30 23:44:19 -0400

    Update TwilioVideoClient.ts

--- pro-00-fe-simple/src/common/twilio/video/TwilioVideoClient.ts
+++ pro-00-fe-simple/src/common/twilio/video/TwilioVideoClient.ts
@@ -1,34 +1,21 @@
-import {Conversation} from "@twilio/conversations/lib/conversation";
-import {Paginator} from "@twilio/conversations/lib/interfaces/paginator";
-import {Message} from "@twilio/conversations/lib/message";
-import twilioClientFactory from "../TwilioClientFactory";
-import {Participant, Room} from "twilio-video";
+import Video, {Room} from "twilio-video";
 import backendVideoClient from "./BackendVideoClient";
-import Video from "twilio-video";
 import authenticationService from "../../../login/AuthenticationService";
 
-/**
- * Guideline:
- * https://www.twilio.com/docs/conversations/sdk-download-install
- * https://www.npmjs.com/package/@twilio/conversations
- */
-const twilioConversationClient = {
+const twilioVideoClient = {
 
-  /**
-   * the participantIdentifier must have an existing User in Twilio.
-   * If there's no corresponding user, it will throw an error.
-   * @param mainUserIdentifier
-   * @param targetParticipantIdentifier
-   */
-  startVideoRoomOneOnOne: async (targetParticipantIdentifier: string): Promise<Room> => {
+  startNewVideoRoomOneOnOne: async (): Promise<Room> => {
     const authenticatedUser = authenticationService.validateAuthenticated();
-
     const videoRoom = await backendVideoClient.createRoom();
-
     const room: Room = await Video.connect(authenticatedUser.twilioAccessToken, {name: videoRoom.uniqueName});
     return room;
   },
 
+  joinVideoRoom: async (roomUniqueName: string): Promise<Room> => {
+    const authenticatedUser = authenticationService.validateAuthenticated();
+    const room: Room = await Video.connect(authenticatedUser.twilioAccessToken, {name: roomUniqueName});
+    return room;
+  },
 
 }
-export default twilioConversationClient;
+export default twilioVideoClient;
