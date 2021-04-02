e2e0de26ddc97afb996f37f75770ccf9aa250444
trank <Kevin.Tran@pointclickcare.com>
2021-04-02 17:16:55 -0400

    Update CSS for participants

--- pro-00-fe-simple/src/video/VideoRoomsList.tsx
+++ pro-00-fe-simple/src/video/VideoRoomsList.tsx
@@ -5,12 +5,15 @@ const VideoRoomsList = (): JSX.Element => {
 
   return (
     <>
-      <div className={'pb-3 pt-3'} style={{textAlign: "center"}}>
+      <div className={'pb-3 pt-3'} style={{textAlign: "center", borderBottom: '1px solid #777'}}>
         Available Rooms
       </div>
-      <div>
-        <button className={'btn btn-outline-secondary w-100'} title={'Room01 Sid SRxxxxxxxxxxxxxxx'}>Room01</button>
+      <div className={'pt-3'}>
+        <button className={'w-100 room-item'} title={'Room01 Sid SRxxxxxxxxxxxxxxx'}>Room01</button>
+        <button className={'w-100 room-item'} title={'Room02 Sid SRxxxxxxxxxxxxxxx'}>Room02</button>
+        <button className={'w-100 room-item'} title={'Room03 Sid SRxxxxxxxxxxxxxxx'}>Room03</button>
       </div>
+
     </>
   );
 };
